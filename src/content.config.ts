07b6993664a0b825a873bf5861600ddc6f6156c4
YuWenHao1212 <wenhao.yu@uni.minerva.edu>
2026-04-09 16:55:06 +0800

    chore: exclude drafts/ from blog content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+  loader: glob({ pattern: ['**/*.{md,mdx}', '!drafts/**'], base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     seoTitle: z.string().optional(),
